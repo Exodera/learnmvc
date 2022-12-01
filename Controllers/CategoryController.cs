12434e1c4cf40fbc7f6159decdc5f6d514dfa80f
Oratile Ditlhage <96663869+Exodera@users.noreply.github.com>
2022-12-01 18:42:07 +0200

    git init

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LearnMVC.Data;
+using LearnMVC.Models;
+
+namespace LearnMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        //get
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //post
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The Display order cant match the name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+    }
+}
